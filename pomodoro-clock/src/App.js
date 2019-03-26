b4a48c8a90d1275c4b92a5ed57e065231008585c
Jesp <jstriker9peru@gmail.com>
2019-03-26 10:27:47 -0400

    Added ids

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -9,32 +9,34 @@ class App extends Component {
           <div><h1>Pomodoro Clock</h1></div>
           <div className="row">
             <div className="box1">
-              <h2>Break Length</h2>
+              <h2 id="break-label">Break Length</h2>
               <div>
-                <i class="fas fa-arrow-down"></i>
-                <h3>5</h3>
-                <i class="fas fa-arrow-up"></i>
+                <i id="break-decrement" class="fas fa-arrow-down"></i>
+                <h3 id="break-length">5</h3>
+                <i id="break-increment" class="fas fa-arrow-up"></i>
               </div>
             </div>
             <div className="box2">
-              <h2>Session Length</h2>
+              <h2 id="session-label">Session Length</h2>
               <div>
-                <i class="fas fa-arrow-down"></i>
-                <h3>25</h3>
-                <i class="fas fa-arrow-up"></i>
+                <i id="session-decrement" class="fas fa-arrow-down"></i>
+                <h3 id="session-length">25</h3>
+                <i id="session-increment" class="fas fa-arrow-up"></i>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="timer-container">
-              <div><h2>Break</h2></div>
-              <div><h2>03:58</h2></div>
+              <div>
+                <h2 id="timer-label">Break</h2>
+              </div>
+              <div><h2 id="time-left">03:58</h2></div>
             </div>
           </div>
           <div className="row">
-            <i class="fas fa-play-circle"></i>
+            <i id="start_stop" class="fas fa-play-circle"></i>
             <i class="fas fa-pause-circle"></i>
-            <i class="fas fa-sync-alt"></i>
+            <i id="reset" class="fas fa-sync-alt"></i>
           </div>
         </div>
       </div>
