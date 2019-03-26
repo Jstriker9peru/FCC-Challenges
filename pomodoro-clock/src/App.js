f748b537d6412e7a89875fbc524890bfbf2775e3
Jesp <jstriker9peru@gmail.com>
2019-03-26 13:04:52 -0400

    Clock styling

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -6,11 +6,13 @@ class App extends Component {
     return (
       <div className="App">
         <div className="clock-container">
-          <div><h1>Pomodoro Clock</h1></div>
-          <div className="row">
+          <div className="row1">
+            <h1>Pomodoro Clock</h1>
+          </div>
+          <div className="row2">
             <div className="box1">
               <h2 id="break-label">Break Length</h2>
-              <div>
+              <div className="change">
                 <i id="break-decrement" class="fas fa-arrow-down"></i>
                 <h3 id="break-length">5</h3>
                 <i id="break-increment" class="fas fa-arrow-up"></i>
@@ -18,25 +20,33 @@ class App extends Component {
             </div>
             <div className="box2">
               <h2 id="session-label">Session Length</h2>
-              <div>
+              <div className="change">
                 <i id="session-decrement" class="fas fa-arrow-down"></i>
                 <h3 id="session-length">25</h3>
                 <i id="session-increment" class="fas fa-arrow-up"></i>
               </div>
             </div>
           </div>
-          <div className="row">
+          <div className="row3">
             <div className="timer-container">
-              <div>
+              <div className="top">
                 <h2 id="timer-label">Break</h2>
               </div>
-              <div><h2 id="time-left">03:58</h2></div>
+              <div className="bottom">
+                <h2 id="time-left">
+                  03
+                  <span className="colon">:</span>
+                  58
+                </h2>
+              </div>
             </div>
           </div>
-          <div className="row">
-            <i id="start_stop" class="fas fa-play-circle"></i>
-            <i class="fas fa-pause-circle"></i>
-            <i id="reset" class="fas fa-sync-alt"></i>
+          <div className="row4">
+            <div className="icon-container">
+              <i id="start_stop" class="fas fa-play-circle"></i>
+              <i class="fas fa-pause-circle"></i>
+              <i id="reset" class="fas fa-sync-alt"></i>
+            </div>
           </div>
         </div>
       </div>
