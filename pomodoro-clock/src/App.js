fe6a550589578698d6d342191c4b6c4479d3e1fd
Jesp <jstriker9peru@gmail.com>
2019-03-26 15:05:03 -0400

    Added timer start and stop functions

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -2,6 +2,54 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timerStarted: false,
+      timerStopped: true,
+      minutes: 0,
+      seconds: 0
+    }
+    this.handleTimerStart = this.handleTimerStart.bind(this);
+    this.handleTimerStop = this.handleTimerStop.bind(this);
+  }
+
+  handleTimerStart() {
+
+    if (this.state.timerStopped) {
+      this.timer = setInterval(() => {
+          this.setState({
+            timerStarted: true,
+            timerStopped: false,
+          });
+
+          if (this.state.timerStarted) {
+            if (this.state.seconds >= 59) {
+              this.setState((prevState) => ({
+                minutes: prevState.minutes + 1,
+                seconds: -1
+              }))
+            };
+
+            this.setState((prevState) => ({
+              seconds: prevState.seconds + 1
+            }));
+          }
+      }, 1000);
+    }
+  }
+
+  handleTimerStop() {
+    if (this.state.timerStarted) {
+      this.setState({
+        timerStarted: false,
+        timerStopped: true,
+        seconds: this.state.seconds
+      })
+      clearInterval(this.timer);
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -34,17 +82,17 @@ class App extends Component {
               </div>
               <div className="bottom">
                 <h2 id="time-left">
-                  03
+                  {this.state.minutes}
                   <span className="colon">:</span>
-                  58
+                  {this.state.seconds}
                 </h2>
               </div>
             </div>
           </div>
           <div className="row4">
             <div className="icon-container">
-              <i id="start_stop" class="fas fa-play-circle"></i>
-              <i class="fas fa-pause-circle"></i>
+              <i id="start_stop" class="fas fa-play-circle" onClick={this.handleTimerStart}></i>
+              <i class="fas fa-pause-circle" onClick={this.handleTimerStop}></i>
               <i id="reset" class="fas fa-sync-alt"></i>
             </div>
           </div>
