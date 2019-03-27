65a4f62e03646ccae286be0a8e32918ecf1c2e2d
Jesp <jstriker9peru@gmail.com>
2019-03-27 11:11:01 -0400

    Added switch functionality

--- pomodoro-clock/src/App.js
+++ pomodoro-clock/src/App.js
@@ -7,15 +7,45 @@ class App extends Component {
     this.state = {
       timerStarted: false,
       timerStopped: true,
+      sessionOn: false,
+      breakOn: true,
       minutes: 0,
-      seconds: 0
+      seconds: 0,
+      breakMin: 5,
+      breakSec: 0,
+      sessionMin: 25,
+      sessionSec: 0
     }
     this.handleTimerStart = this.handleTimerStart.bind(this);
     this.handleTimerStop = this.handleTimerStop.bind(this);
+    this.handleTimerRestart = this.handleTimerRestart.bind(this);
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+    this.handleSwitch = this.handleSwitch.bind(this);
+  }
+  componentWillMount() {
+    this.handleTimerRestart();
   }
 
-  handleTimerStart() {
+  handleSwitch() {    
+    this.setState((prevState) => ({
+      sessionOn: !prevState.sessionOn,
+      breakOn: !prevState.breakOn,
+      timerStarted: false,
+      timerStopped: true,
+    }));
+    setTimeout(() => {
+      this.handleTimerRestart();
+    }, 1);
 
+  }
+
+  handleSwitchButton() {}
+
+  handleTimerStart() {
+    if (this.state.sessionOn) {
+
+    }
     if (this.state.timerStopped) {
       this.timer = setInterval(() => {
           this.setState({
@@ -24,18 +54,26 @@ class App extends Component {
           });
 
           if (this.state.timerStarted) {
-            if (this.state.seconds >= 59) {
+            if (this.state.seconds <= 0) {
               this.setState((prevState) => ({
-                minutes: prevState.minutes + 1,
-                seconds: -1
-              }))
+                minutes: prevState.minutes - 1,
+                seconds: 60
+              }));
             };
 
             this.setState((prevState) => ({
-              seconds: prevState.seconds + 1
+              seconds: prevState.seconds - 1
             }));
+            if (this.state.minutes === 0 &&
+              this.state.seconds === 0) {
+                clearInterval(this.timer);
+                setTimeout(() => {
+                  this.handleSwitch();      
+                  this.handleTimerStart();
+                }, 1000);
+            }
           }
-      }, 1000);
+      }, 100);
     }
   }
 
@@ -50,6 +88,104 @@ class App extends Component {
     }
   }
 
+  handleTimerRestart() {
+    if (this.state.sessionOn) {
+
+      this.setState({
+        seconds: this.state.sessionSec,
+        minutes: this.state.sessionMin
+      });
+    } else if (this.state.breakOn) {
+
+      this.setState({
+        seconds: this.state.breakSec,
+        minutes: this.state.breakMin
+      });
+    }
+  }
+
+  handleIncrement(type) {
+    if (this.state.timerStopped) {
+      if (this.state.sessionOn && type === 'sess') {
+        if (this.state.sessionMin <= 59) {
+
+          this.handleTimerRestart();
+          this.setState((prevState) => ({
+            sessionMin: prevState.sessionMin + 1,
+            minutes: prevState.minutes + 1
+          }));
+        }
+        
+      } else if (this.state.breakOn && type === 'break') {
+          if (this.state.breakMin <= 59) {
+            this.handleTimerRestart();
+            this.setState((prevState) => ({
+              breakMin: prevState.breakMin + 1, 
+              minutes: prevState.minutes + 1
+            }));
+          }
+        }
+    } else {
+
+
+      if (this.state.sessionOn && type === 'sess') {
+        if (this.state.sessionMin <= 59) {
+
+          this.setState((prevState) => ({
+            sessionMin: prevState.sessionMin + 1
+          }));
+        }
+        
+      } else if (this.state.breakOn && type === 'break') {
+          if (this.state.breakMin <= 59) {
+            this.setState((prevState) => ({
+              breakMin: prevState.breakMin + 1
+            }));
+          }
+        }
+    }
+
+  }
+
+  handleDecrement(type) {
+    if (this.state.timerStopped) {
+      if (this.state.sessionOn && type === 'sess') {
+        if (this.state.sessionMin >= 2) {
+          this.handleTimerRestart();
+          this.setState((prevState) => ({
+            sessionMin: prevState.sessionMin - 1,
+            minutes: prevState.minutes - 1 
+          }))
+        }
+      } else if (this.state.breakOn && type === 'break') {
+        if (this.state.breakMin >= 2) {
+          this.handleTimerRestart();
+          this.setState((prevState) => ({
+            breakMin: prevState.breakMin - 1,
+            minutes: prevState.minutes - 1 
+          }))
+        }
+      }
+    } else {
+
+      if (this.state.sessionOn && type === 'sess') {
+        if (this.state.sessionMin >= 2) {
+          this.setState((prevState) => ({
+            sessionMin: prevState.sessionMin - 1
+          }))
+        }
+      } else if (this.state.breakOn && type === 'break') {
+        if (this.state.breakMin >= 2) {
+          this.setState((prevState) => ({
+            breakMin: prevState.breakMin - 1
+          }))
+        }
+      }
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
@@ -61,29 +197,34 @@ class App extends Component {
             <div className="box1">
               <h2 id="break-label">Break Length</h2>
               <div className="change">
-                <i id="break-decrement" class="fas fa-arrow-down"></i>
-                <h3 id="break-length">5</h3>
-                <i id="break-increment" class="fas fa-arrow-up"></i>
+                <i id="break-decrement" className="fas fa-arrow-down" onClick={() => this.handleDecrement('break')}></i>
+                <h3 id="break-length">{this.state.breakMin}</h3>
+                <i id="break-increment" className="fas fa-arrow-up" onClick={() => this.handleIncrement('break')}></i>
               </div>
             </div>
             <div className="box2">
               <h2 id="session-label">Session Length</h2>
               <div className="change">
-                <i id="session-decrement" class="fas fa-arrow-down"></i>
-                <h3 id="session-length">25</h3>
-                <i id="session-increment" class="fas fa-arrow-up"></i>
+                <i id="session-decrement" className="fas fa-arrow-down" onClick={() => this.handleDecrement('sess')}></i>
+                <h3 id="session-length">{this.state.sessionMin}</h3>
+                <i id="session-increment" className="fas fa-arrow-up" onClick={() => this.handleIncrement('sess')}></i>
               </div>
             </div>
           </div>
           <div className="row3">
             <div className="timer-container">
               <div className="top">
-                <h2 id="timer-label">Break</h2>
+                <h2 id="timer-label">
+                  {this.state.sessionOn ? 'Session' : ''}
+                  {this.state.breakOn ? 'Break' : ''}
+                </h2>
               </div>
               <div className="bottom">
                 <h2 id="time-left">
+                  {this.state.minutes < 10 ? 0 : ''}
                   {this.state.minutes}
                   <span className="colon">:</span>
+                  {this.state.seconds < 10 ? 0 : ''}
                   {this.state.seconds}
                 </h2>
               </div>
@@ -91,9 +232,28 @@ class App extends Component {
           </div>
           <div className="row4">
             <div className="icon-container">
-              <i id="start_stop" class="fas fa-play-circle" onClick={this.handleTimerStart}></i>
-              <i class="fas fa-pause-circle" onClick={this.handleTimerStop}></i>
-              <i id="reset" class="fas fa-sync-alt"></i>
+
+              <span title="start">
+                <i 
+                id="start_stop" 
+                className="fas fa-play-circle" 
+                onClick={this.handleTimerStart}></i>
+              </span>
+              <span title="stop">
+                <i 
+                className="fas fa-pause-circle" 
+                onClick={this.handleTimerStop}></i>
+              </span>
+              <span title="reset">
+                <i 
+                id="reset" 
+                className="fas fa-sync-alt" 
+                onClick={this.handleTimerRestart}></i> 
+              </span>
+              <span title="switch">
+                <i className="fas fa-exchange-alt" onClick={this.handleSwitch}></i>
+              </span>
+              
             </div>
           </div>
         </div>
